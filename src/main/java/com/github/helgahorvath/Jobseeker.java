df8341fc35b0cfb1908f49603645fb2ef2430b63
helgahorvath <h.horvath302@gmail.com>
2020-04-27 00:24:59 +0200

    initial commit

---
+++ src/main/java/com/github/helgahorvath/Jobseeker.java
@@ -0,0 +1,12 @@
+package com.github.helgahorvath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobseeker {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Jobseeker.class);
+  }
+}
