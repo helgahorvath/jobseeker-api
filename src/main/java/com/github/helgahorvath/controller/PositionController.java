0feb005b56710b8e3e08af1fe02fb4d1a86429e6
helgahorvath <h.horvath302@gmail.com>
2020-04-28 13:04:45 +0200

    validation for getpositions keywords

--- src/main/java/com/github/helgahorvath/controller/PositionController.java
+++ src/main/java/com/github/helgahorvath/controller/PositionController.java
@@ -5,11 +5,15 @@ import com.github.helgahorvath.service.ClientService;
 import com.github.helgahorvath.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @RestController
+@RequestMapping("positions")
+@Validated
 public class PositionController {
 
   private PositionService positionService;
@@ -21,7 +25,7 @@ public class PositionController {
     this.clientService = clientService;
   }
 
-  @PostMapping("/positions")
+  @PostMapping
   public String createPosition(@RequestBody Position position, HttpServletRequest request) {
     throwExceptionIfUnauthorized(request);
     StringBuffer requestUrl = request.getRequestURL().append("/");
@@ -30,10 +34,15 @@ public class PositionController {
     return position.getUrl();
   }
 
-  @GetMapping("/positions")
-  public List<Position> getPositions(@RequestParam String jobTitle, String location, HttpServletRequest request) {
+  @GetMapping
+  public List<Position> getPositions(@RequestParam
+                                     @Size(min = 3, max = 50, message = "Please enter a keyword between 3 and 50 characters")
+                                     String keyword,
+                                     @Size(min = 3, max = 50, message = "Please enter a location between 3 and 50 characters")
+                                     String location,
+                                     HttpServletRequest request) {
     throwExceptionIfUnauthorized(request);
-    return positionService.getPositionList(jobTitle, location);
+    return positionService.getPositionList(keyword, location);
   }
 
   private void throwExceptionIfUnauthorized (HttpServletRequest request) {
